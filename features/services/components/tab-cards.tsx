4ea214b479c6afbf17c1675ec556e5734f6a3153
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-13 15:04:38 -0300

    Enhance TabCards component by adding new icons for service details and improving layout with responsive design; update CardContent to display service time and price more effectively.

--- features/services/components/tab-cards.tsx
+++ features/services/components/tab-cards.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { frontendServices, backendServices, integrationsServices, customFeaturesServices } from "@/features/services/lib/utils";
 import { TabCardProps } from "@/features/services/types";
 import { Button } from "@/features/ui/components/button";
-import { Plus } from "lucide-react";
+import { CircleDollarSign, Clock, DollarSign, Plus } from "lucide-react";
 
 const TabCards = ({ service }: TabCardProps) => {
 
@@ -18,13 +18,18 @@ const TabCards = ({ service }: TabCardProps) => {
     return (
         <div className="grid grid-cols-[repeat(auto-fill,minmax(min(300px,100%),1fr))] gap-4 max-w-screen-lg mx-auto">
             {services.map((service) => (
-                <Card key={service.id}>
+                <Card key={service.id} className="border-muted-foreground/20 hover:border-accent hover:scale-105 transition-all">
                     <CardHeader>
-                        <CardTitle>{service.title}</CardTitle>
+                        <CardTitle className="text-xl font-bold">{service.title}</CardTitle>
                         <CardDescription className="line-clamp-2">{service.description}</CardDescription>
                     </CardHeader>
-                    <CardContent>
-
+                    <CardContent className="flex justify-between">
+                        <p className="text-sm text-muted-foreground flex items-center gap-2">
+                            <Clock className="size-4"/> {service.timeHours} hs
+                        </p>
+                        <p className="text-sm text-muted-foreground flex items-center gap-2">
+                            USD ${service.basePrice}
+                        </p>
                     </CardContent>
                     <CardFooter>
                         <Button className="w-full cursor-pointer hover:bg-accent">
