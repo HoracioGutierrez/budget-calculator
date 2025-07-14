8cad026ca2673d304b185f9e8a73bbef079104b7
Horacio Gutierrez <horacio.estevez@gmail.com>
2025-07-13 21:01:32 -0300

    Refactor Cart component to enhance layout and responsiveness; integrate auto-animate for smoother item transitions and improve item display with title, description, and remove button.

--- features/cart/components/cart.tsx
+++ features/cart/components/cart.tsx
@@ -1,17 +1,36 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/features/ui/components/card";
+"use client"
 
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/features/ui/components/card";
+import { useCart } from "@/features/providers/components/cart-prodiver";
+import { Button } from "@/features/ui/components/button";
+import { Trash2 } from "lucide-react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 const Cart = () => {
+
+    const { cart } = useCart()
+    const [parent] = useAutoAnimate()
+
     return (
         <Card className="hidden md:block border-muted-foreground/20 min-w-sm">
             <CardHeader>
                 <CardTitle>Cart</CardTitle>
             </CardHeader>
-            <CardContent>
-                <div className="flex flex-col gap-4">
-                    <div className="flex flex-col gap-2">
-                        <p className="text-sm text-muted-foreground">Item</p>
-                    </div>
-                </div>
+            <CardContent ref={parent} className="flex flex-col gap-2">
+                {cart.map((item) => (
+                    <Card key={item.id} className="border-none bg-secondary">
+                        <CardHeader className="flex flex-row justify-between items-center">
+                            <div>
+                                <CardTitle className="text-base">{item.title}</CardTitle>
+                                <CardDescription className="text-sm text-muted-foreground">
+                                    ${item.basePrice} x 1
+                                </CardDescription>
+                            </div>
+                            <Button variant="ghost" size="icon" className="cursor-pointer hover:bg-red-400 hover:text-destructive-foreground">
+                                <Trash2 />
+                            </Button>
+                        </CardHeader>
+                    </Card>
+                ))}
             </CardContent>
         </Card>
     );
